dcfcae46c6ceab055237e188f1ead68bff154075
Michał Puchyr <michalpuchyr3@gmail.com>
2024-12-19 21:46:55 +0100

    feat: Add vue template project

---
+++ Didactica.UI/src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-btn color="primary">Click Me</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
